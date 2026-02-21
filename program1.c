892ab1c9353c8bdcd8b8b07f4c8a5cb702f207aa
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-02-21 23:31:26 +0530

    added first c program

---
+++ program1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    // calculate marks of 5 subject
+    int a = 5 , b = 6 , d = 3 , c = 4 , e = 4 , sum;
+    sum = a + b + c + d + e;
+    printf("the sum of marks of 5 subject is %d" , sum );
+    return 0;
+
+}
