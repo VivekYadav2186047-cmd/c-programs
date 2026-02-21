6a4ed236a93e0a4fe5e43b95d2e17a4ec8600f2a
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-02-21 23:33:07 +0530

    added 2nd c program

---
+++ program1part2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    // calculate the sum of marks of subject
+    int a , b , c ,  sum ;
+    printf("enter a = " );
+     scanf("%d" , &a);
+
+     printf("enter b = ");
+     scanf("%d" , &b);
+     printf("enter c =");
+     scanf("%d" , &c);
+     printf("the sum of value of %d , %d , and %d is %d" , a , b , c , a+b+c);
+     return 0;
+
+}
