66e2dae250f3c5e2961256070914b46dd2316d10
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-02-21 23:35:57 +0530

    added 4th c program

---
+++ program4.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+
+int main(){
+    // to check value is odd or even
+    int a;
+    printf("enter a values = ");
+    scanf("%d" , &a);
+    if(a%2==0){
+        printf("value is even ");
+
+    }
+    else{
+        printf("value is odd");
+    }
+    return 0;
+
+}
