b602582e70297d3e99031baada06d8380e1864bd
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-02-22 16:40:14 +0530

    Fix formatting and add missing newline at end of file

--- adding marks of 5 subject .c
+++ adding marks of 5 subject .c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-int main(){
+int 
+main(){
     // calculate marks of 5 subject
     int a = 5 , b = 6 , d = 3 , c = 4 , e = 4 , sum;
     sum = a + b + c + d + e;
     printf("the sum of marks of 5 subject is %d" , sum );
     return 0;
+
 
 }
