7584a85fb5d9b94c3e1232c07d09dd35562e3315
VivekYadav2186047-cmd <vy2186047@gmail.com>
2026-03-23 00:59:47 +0530

    Add files via upload
    for loop .c

---
+++ 09_for.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    //Write a program to print first ‘n’ natural numbers using for loop
+    int n = 12;
+    for(int i = 1; i<=n; i++){
+        printf(" %d\n" , i);
+    }
+    return 0;
+}
